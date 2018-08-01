aedcd44bad7b4f0b7b98eac28b8c6b27c72c8511
Ben Greear <greearb@candelatech.com>
2018-08-01 23:18:50 +0000

    apply backport patch 0016-libertas-olpc-ec-wakeup/drivers_net_wireless_libertas_if_usb.patch

--- drivers/net/wireless/marvell/libertas/if_usb.c
+++ drivers/net/wireless/marvell/libertas/if_usb.c
@@ -939,6 +939,7 @@ static int if_usb_suspend(struct usb_interface *intf, pm_message_t message)
 		goto out;
 	}
 
+#if LINUX_VERSION_IS_GEQ(3,1,0)
 #ifdef CONFIG_OLPC
 	if (machine_is_olpc()) {
 		if (priv->wol_criteria == EHS_REMOVE_WAKEUP)
@@ -946,6 +947,7 @@ static int if_usb_suspend(struct usb_interface *intf, pm_message_t message)
 		else
 			olpc_ec_wakeup_set(EC_SCI_SRC_WLAN);
 	}
+#endif
 #endif
 
 	ret = lbs_suspend(priv);
