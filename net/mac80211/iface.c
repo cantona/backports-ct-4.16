ce9927ac7defea595ad8f070f059d07182c89d7a
Ben Greear <greearb@candelatech.com>
2018-08-01 23:37:12 +0000

    apply backport SmPL patch 0079-netdev-destructor.cocci

--- net/mac80211/iface.c
+++ net/mac80211/iface.c
@@ -1310,13 +1310,19 @@ static void ieee80211_if_free(struct net_device *dev)
 	free_percpu(netdev_tstats(dev));
 }
 
+#if LINUX_VERSION_IS_LESS(4,12,0)
+static void __ieee80211_if_free(struct net_device *ndev){
+	ieee80211_if_free(ndev);
+	free_netdev(ndev);
+}
+#endif
+
 static void ieee80211_if_setup(struct net_device *dev)
 {
 	ether_setup(dev);
 	dev->priv_flags &= ~IFF_TX_SKB_SHARING;
 	dev->netdev_ops = &ieee80211_dataif_ops;
-	dev->needs_free_netdev = true;
-	dev->priv_destructor = ieee80211_if_free;
+	netdev_set_priv_destructor(dev, ieee80211_if_free);
 }
 
 static void ieee80211_if_setup_no_queue(struct net_device *dev)
@@ -1991,6 +1997,9 @@ int ieee80211_if_add(struct ieee80211_local *local, const char *name,
 
 		ret = register_netdevice(ndev);
 		if (ret) {
+#if LINUX_VERSION_IS_LESS(4,12,0)
+			ieee80211_if_free(ndev);
+#endif
 			free_netdev(ndev);
 			return ret;
 		}
